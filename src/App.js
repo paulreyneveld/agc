6a0df72b29503b00993191427dcf6ee437986465
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-25 21:58:23 -0800

    Created first route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom'
 import Navigation from './components/Navigation'
 import TestBody from './components/TestBody'
 import Footer from './components/Footer'
@@ -7,9 +12,16 @@ import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-    <Navigation />
-    <TestBody />
-    <Footer />
+    <Router>
+      <Navigation />
+        <Switch>
+          <Route path="/">
+            <TestBody />
+          </Route>
+          
+        </Switch>
+      <Footer />
+    </Router>
     </>
   )
 }
