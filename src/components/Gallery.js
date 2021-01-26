93d1e67dbe5ab65b47fd9c639a8281982659b0a6
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-26 14:00:59 -0800

    Added dummy images via api to gallery component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
+import axios from 'axios'
 
 const Gallery = () => {
+
+    const [dummyImages, setDummyImages] = useState([])
+
+    useEffect(() => {
+        axios.get('https://picsum.photos/v2/list?page=1&limit=30/200/300')
+            .then(response => setDummyImages(response.data))
+    }, [])
+    console.log(dummyImages)
+
     return (
         <>
         <Container>
