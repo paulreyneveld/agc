347632f8f2c97d54184a769be8bb03717d1b3aa3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-28 14:28:47 -0800

    Messing around with gallery layout

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,17 +13,33 @@ const Gallery = () => {
             <Col xs={6} md={4}>
             <Image src="https://picsum.photos/300/200?random=1" thumbnail />
             </Col>
-            <Col xs={6} md={4}>
+            <Col xs={6} md={8}>
             <Image src="https://picsum.photos/300/200?random=2" thumbnail />
             </Col>
             <Col xs={6} md={4}>
             <Image src="https://picsum.photos/300/200?random=3" thumbnail />
             </Col>
-        </Row>
-        <Row>
             <Col xs={6} md={4}>
             <Image src="https://picsum.photos/300/200?random=4" thumbnail />
             </Col>
+            <Col xs={6} md={4}>
+            <Image src="https://picsum.photos/300/200?random=5" thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+            <Image src="https://picsum.photos/300/200?random=6" thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+            <Image src="https://picsum.photos/300/200?random=7" thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+            <Image src="https://picsum.photos/300/200?random=8" thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+            <Image src="https://picsum.photos/300/200?random=9" thumbnail />
+            </Col>
+        </Row>
+        <Row>
+
         </Row>
         </Container>
         </>
