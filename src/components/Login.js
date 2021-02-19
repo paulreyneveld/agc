b92703c639060a963d647f48777f0074b60c8105
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-18 19:17:29 -0800

    Fleshed out jwt functionality from front to back end

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
+import loginService from '../services/login'
 
 const Login = () => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
-    const handleLogin = (event) => {
+    const handleLogin = async (event) => {
         event.preventDefault()
-        console.log('logging in with', username, password)
-      }
+        try {
+            const user = await loginService.login({
+              username, password,
+            })
+            window.localStorage.setItem(
+                'loggedAdmin', JSON.stringify(user)
+            )
+            setUser(user)
+            setUsername('')
+            setPassword('')
+          } catch (exception) {
+            // setErrorMessage('Wrong credentials')
+            // setTimeout(() => {
+            //   setErrorMessage(null)
+            // }, 5000)
+          }
+    }
 
     return (
         <>
