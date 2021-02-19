1db01cf25fa7524748321c20d0d9e25a9a0e4aec
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-18 19:01:20 -0800

    Added frontend markup for login ability

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+        console.log('logging in with', username, password)
+      }
 
     return (
         <>
         <h1>Hello from login</h1>
+        <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
         </>
     )
 }
