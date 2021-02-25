7963e951d79cb6397afbed512c3c636d391ab9e8
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-24 16:11:12 -0800

    Adding support for redux state management

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
