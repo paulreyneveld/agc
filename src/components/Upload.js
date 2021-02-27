73db95dcb3bb8550eb19c19d08b586d07d43c4be
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-26 19:01:39 -0800

    Added an upload form

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 const Upload = () => {
     return (
-        <p>hello from Upload</p>
+        <Container>
+        <Form
+        // onSubmit={handleFormSubmit}
+        method="post"
+        encType="multipart/form-data"
+        className="upload-form"
+      >
+        <Form.Group>
+          <Form.Label>Choose photo to upload</Form.Label>
+          {/* <Form.Control type="file" name="photo" onChange={handleOnChange} /> */}
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+        >
+          Upload
+        </Button>
+      </Form>
+      </Container>
     )
 }
 
