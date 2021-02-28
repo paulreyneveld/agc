e875d90338931fcc2f22b9dbded75f7b7a095263
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-27 21:23:28 -0800

    Fixed Footer warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
-import { Link } from 'react-router-dom'
 
 const Footer  = () => {
     return (
