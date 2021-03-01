fe8f2b08aa258f2d8cd18f1124486fa955653b10
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-28 19:30:22 -0800

    Verified access to the photos on the server

--- src/components/TestGallery.js
+++ src/components/TestGallery.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { BASE_API_URL } from '../utils/constants'
+import Container from 'react-bootstrap/esm/Container'
+import Photo from './Photo'
 
 const TestGallery = () => {
-    console.log(BASE_API_URL)
+
+    useEffect(() => {
+        axios.get(`${BASE_API_URL}/photos`)
+            .then(response => console.log(response))
+    }, [])
+
+
     return (
-        <p>cheese</p>
+        <Container>
+            <p>cheese</p>
+        </Container>
     )
 }
 
