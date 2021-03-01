adabe468ed695514aeda90fe668245133dd5a84b
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-28 19:18:11 -0800

    Added a TestGallery component to test the backend functionality of the image upload

---
+++ src/components/TestGallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+import BASE_API_URL from '../utils/constants'
+
+const TestGallery = () => {
+    console.log(BASE_API_URL)
+    return (
+        <p>cheese</p>
+    )
+}
+
+export default TestGallery
