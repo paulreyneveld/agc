ba94b1ee515d6923bd77bd80a1cee6091e7aa528
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-01 17:13:56 -0800

    Finished TestGallery frontend functionality

--- src/components/TestGallery.js
+++ src/components/TestGallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { BASE_API_URL } from '../utils/constants'
 import Container from 'react-bootstrap/esm/Container'
@@ -6,15 +6,22 @@ import Photo from './Photo'
 
 const TestGallery = () => {
 
+    const [photos, setPhotos] = useState([])
+    
     useEffect(() => {
         axios.get(`${BASE_API_URL}/photos`)
-            .then(response => console.log(response))
+            .then(response => {
+                console.log(response)
+                setPhotos(response.data)
+            })
+            .catch(error => console.log(error))
     }, [])
 
-
+    console.log(photos)
     return (
         <Container>
             <p>cheese</p>
+            {photos.map((photo) => <Photo key={photo._id} id={photo._id} />)}
         </Container>
     )
 }
