f0b972f9f182d689894243eeae680ea307af0d57
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-02 17:48:35 -0800

    Added a blogReducer

---
+++ src/reducers/blogReducer.js
@@ -0,0 +1,10 @@
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case "GET_BLOGS":
+            return state.blogs
+        default: 
+            return state
+    }
+}
+
+export default blogReducer
