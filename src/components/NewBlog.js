06cce260c5799028cfece51238cdd2d367901e26
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-03 20:09:00 -0800

    Fleshed out NewBlog component

--- src/components/NewBlog.js
+++ src/components/NewBlog.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const NewBlog = () => {
     return (
         <Container>
-            <h1>Hello</h1>
+            <h1>New Blog</h1>
+            <Form>
+                <Form.Group controlId="formNewBlog">
+                    <Form.Label>Title:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Blog</Form.Label>
+                    <Form.Control type="textarea" placeholder="Type blog here. . ." />              
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                </Form>
         </Container>
     )
 }
