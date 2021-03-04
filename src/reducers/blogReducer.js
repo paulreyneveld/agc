2035a887d31fc58c6380f7859b498ad7c9caa2b3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-03 21:17:14 -0800

    Added an action for the blogReducer

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -1,9 +1,24 @@
+import axios from "axios"
+
+import BASE_API_URL from '../utils/config'
+
 const blogReducer = (state = [], action) => {
     switch(action.type) {
         case "GET_BLOGS":
             return state.blogs
         default: 
             return state
+    }
+}
+
+export const initializeBlogs = () => {
+    return async dispatch => {
+        // const blogs = await blogService.getAll()
+        const blogs = await axios.get(`${BASE_API_URL}/api/blogs`)
+        dispatch({
+            type: 'GET_BLOGS',
+            data: blogs
+        })
     }
 }
 
