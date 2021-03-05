30d09460d2df3ddc13e28d2de36247ba8d4dff59
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-04 16:33:48 -0800

    Added support for markdown in the Blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import { initializeBlogs } from '../reducers/blogReducer'
 import { useDispatch, useSelector } from 'react-redux'
+import ReactMarkdown from 'react-markdown'
 
 const Blog = () => {
 
