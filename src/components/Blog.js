d1977e29df2b203a912ff4ad52382c9f66f6c04b
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-07 20:25:05 -0800

    Added inline styling for blog posts

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -22,12 +22,20 @@ const Blog = (props) => {
     const editBlog = (id) => {
         dispatch(updateBlog(id))
     }
+
+    const blogStyle = {
+        borderStyle: "solid",
+        borderColor: "#FFEA8E",
+        borderRadius: "1em",
+        padding: "1em",
+        marginBottom: "1em"
+    }
     return (
         <Container>
             <h1>Blog</h1>
             {blogs && blogs.slice(0).reverse().map(blog => {
                 return (
-                    <div key={blog.id}>
+                    <div key={blog.id} style={blogStyle}>
                     <h3 >{blog.title}</h3>
                     <Button onClick={() => removeBlog(blog.id)}>Delete Post</Button>
                     <Button onClick={() => editBlog(blog.id)}>Edit</Button>
