6e957b0fe547f928e104ebeafc00a4b6bbb09a23
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-15 20:40:00 -0700

    Added an imageReducer to manage state for this alternate solution

---
+++ src/reducers/imageReducer.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { BASE_API_URL } from '../utils/constants';
+
+const imageReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'LOAD_IMAGES':
+      return action.images
+    default:
+      return state
+  }
+}
+
+export default imageReducer
+
