4bfdae84bf1193996a8568992da74bec1ee0151f
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-21 16:12:54 -0700

    Added a NewFooter component

---
+++ src/components/NewFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+
+const NewFooter  = () => {
+    return (
+        <footer>
+
+            Copyright Alien Girl Cult 2021 <br />
+
+        </footer>
+    )
+}
+
+export default NewFooter
