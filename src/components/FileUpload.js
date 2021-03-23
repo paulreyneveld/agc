9475e446d6fa1848fb741ba23dddc63c986a24c4
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-23 13:49:21 -0700

    Added a new FileUpload component

---
+++ src/components/FileUpload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+const FileUpload = () => {
+    return (
+        <Container>
+            <p>helloworld</p>
+        </Container>
+    )
+}
+
+export default FileUpload
