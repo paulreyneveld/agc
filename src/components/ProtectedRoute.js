d1aacc36e36ab95e02cbe22d917dfe9bf8db5c46
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-04-04 18:43:18 -0700

    Added a ProtectedRoute component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+const ProtectedRoute = ({ component: Component, ...rest}) => {
+    return (
+        <Route {...rest} render={ props => <Component {...rest} {...props} /> } /> 
+    )
+}
+
+export default ProtectedRoute
